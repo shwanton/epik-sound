4c6d11a523e0e4059e58b6932b1279c968db55c5
Shawn Dempsey <shwanton@gmail.com>
2017-11-26 16:54:47 -0800

    Build the middleman app first

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo $EPIKSOUND_FTP_HOSTNAME
-echo $EPIKSOUND_FTP_USER
-echo $EPIKSOUND_FTP_PASSWORD
+bundle exec middleman build
 
 lftp -u $EPIKSOUND_FTP_USER,$EPIKSOUND_FTP_PASSWORD $EPIKSOUND_FTP_HOSTNAME -e 'set ftp:ssl-allow no; mirror --reverse --verbose ./build/ /; ls; quit'
